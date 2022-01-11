eac87e7118b13963e5303f6de3f9b9ede5bed510
finnkauski <iwiivi@gmail.com>
2022-01-11 19:14:42 +0000

    Refactor - detach exporter from table
    This starts removing the data table from the export and splitting
    exporters out.

--- tisch/__init__.py
+++ tisch/__init__.py
@@ -1,17 +1,81 @@
+# third party
 import pandas as pd
 
 
-
+@pd.api.extensions.register_dataframe_accessor("tisch")
 class Table:
+    """This represents all the individual parts of the table.
 
-    def __init__(self, data):
-        self.frame = data
-        self.data_html = data.to_html()
+    Within this object the entire state of the additional parts
+    of the table to be exported is stored. Things like titles,
+    subtitles, what cells to merge etc.
 
-    @property
-    def html(self):
-        return self.data_html
+    This is then used by one of the implemented exporters as a
+    standard way to acquire relevant information.
+    """
 
-    def to_file(self, filepath):
-        with open(filepath, "w+") as handle:
-            handle.write(self.html)
+    def __init__(self, pandas_df):
+        # TODO: add data setter
+        self.data = pandas_df
+        self._setup()
+
+    def _setup(self):
+
+        self.title = None
+        self.subtitle = None
+        self.header = None
+        self.footer = None
+        self.source = None
+
+        # Intialise this here as to not have the class list that stuff gets added
+        # to. Common trap with mutable lists as defaults
+        self.merge_operations = []
+
+        self.row_groups = []
+
+    def add_source(self, text):
+        self.source = text
+
+    def add_title(self, text):
+        self.title = text
+
+    def add_subtitle(self, text):
+        # TODO: could support direct tag addition rather than just text
+        if self.title is None:
+            # TODO: add custom errors
+            raise ValueError(
+                "Could not find a title, add title using `.add_title` first."
+            )
+        self.subtitle = text
+
+    def add_rowgroup(self, after_row):
+        self.row_groups.append(after_row)
+
+    def merge_cells(self, row, first, last):
+        # TODO: need to add validation to make sure cells are adjascent
+        # TODO: avoid this kind of thing, implement this in the mergecells
+        cells = list(range(first, last + 1))
+        cells = list(zip([row] * len(cells), cells))
+
+        self.merge_operations.append(cells)
+
+    def reset(self):
+        self._setup()
+
+    def __repr__(self):
+        string = f"""Table:
+------
+title: {self.title}
+subtitle: {self.subtitle}
+header: {self.header}
+footer: {self.footer}
+merges: {self.merge_operations}
+row_groups_after: {self.row_groups}
+-----
+source: {self.source}
+        """
+        return string
+
+
+# TODO: consider subclassing tags into custom tags - like rowgroup
+# TODO: add a - from config json so that someone can have a default json parameterisation of the pipeline
